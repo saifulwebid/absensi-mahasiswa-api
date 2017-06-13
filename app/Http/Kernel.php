374f97433ba4c8863d2d6909e38b6ba6cf4ab699
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-14 05:12:34 +0700

    Remove CORS middleware CORS is already defined in web.config

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Cors::class,
         ],
 
         'api' => [
@@ -51,13 +50,11 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        // 'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
